0af4d94af02219d4959d6f3efa321779ccd92e0c
Iván Wolf <ivanwofl15@gmail.com>
2024-06-19 00:05:26 -0400

    add basic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,md,liquid,erb,serb,rb}',
-    './frontend/javascript/**/*.js',
+    "./src/**/*.{html,md,liquid,erb,serb,rb}",
+    "./frontend/javascript/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#28A745",
+        secondary: "#FFC107",
+        background: "#F8F9FA",
+        dark: "#343A40",
+      },
+    },
   },
   plugins: [],
-}
-
+};
